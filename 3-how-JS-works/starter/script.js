48fe7cfc01bad24ee2d2d98f322e431cf988f14d
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2020-08-05 19:27:32 +0900

    section03_fin

---
+++ 3-how-JS-works/starter/script.js
@@ -0,0 +1,72 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function () {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        /* 
+        // john Obj안에 작성되었지만 regularFn이라서 john 객체가 아닌 window객체를 가리킴
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction(); 
+        */
+    },
+};
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
+    calculateAge: john.calculateAge,
+};
+mike.calculateAge();
+console.log(mike);
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+///////////////////////////////////////
+// Lecture: The this keyword
