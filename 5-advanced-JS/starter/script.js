b45b3e0d6ea30ab4bfba68cd6e95ea62af263de3
seastory624@gmail.com <54757435+sung-woo-jang@users.noreply.github.com>
2020-08-12 03:22:20 +0900

    callback function
    Lecture : Passing functions as arguments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -89,13 +89,15 @@ function change(a, b) {
 
 change(age, obj);
 
-console.log(age);
-console.log(obj.city);
+console.log(age);           // 30
+console.log(obj.city);      // San Francisco
  */
 
 //  Lecture : Passing functions as arguments
+// 함수가 다른 함수를 인수로 받아들이는걸 보자
 var years = [1990, 1965, 1937, 2005, 1998];
 
+// fn == 콜백 함수가 될 매개변수
 function arrayCalc(arr, fn) {
     var arrRes = [];
     for (var i = 0; i < arr.length; i++) {
@@ -112,8 +114,22 @@ function isFullAge(el) {
     return el >= 18;
 }
 
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - 0.67 * el);
+    } else {
+        return -1;
+    }
+}
+
 var ages = arrayCalc(years, calculateAge);
+// 함수()가 아닌 그얄 쓰는게 콜백함수임 ↑
+// 지금이 아닌 arrayClac 함수에 의해 호출되길 원한다는 의도
+// 이렇게 나중에 호출되기 때문에 콜백함수라고 불림
+
 var fullAges = arrayCalc(ages, isFullAge);
-console.log(ages);
-console.log(fullAges);
-// 콜백함수공부하면서 선언식이랑 표현식 복습하자
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages); // [26, 51, 79, 11, 18]
+console.log(fullAges); // [true, true, true, false, true]
+console.log(rates); // [189, 173, 154, -1, 195]
